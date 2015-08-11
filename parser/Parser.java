4fb69c15ff5e3842cab20636374cf7342a83aac2
zhangjie247 <zhangjie247@hotmail.com>
2015-08-11 19:31:28 +0800

    asdfasdf

--- parser/Parser.java
+++ parser/Parser.java
@@ -1,8 +1,8 @@
 package parser;
 
-import lexer.Lexer;
-import lexer.Token;
-import symbols.Env;
+import inter.*;
+import lexer.*;
+import symbols.*;
 
 import java.io.IOException;
 
@@ -16,9 +16,50 @@ public class Parser {
     int used = 0;
     public Parser(Lexer l) throws IOException{lex = l; move();}
     void move() throws IOException{ look = lex.scan();}
-    void error(String s){throw new Error("near line " + lex.line + ": " + s);}
+    void error(String s){throw new Error("near line " + Lexer.line + ": " + s);}
     void match(int t) throws IOException{
         if (look.tag == t) move();
         else error("syntax error");
+    }
+    public void program() throws IOException{
+        Stmt s = block();
+        int begin = s.newlabel();
+        int after = s.newlabel();
+        s.emitlabel(begin);
+        s.gen(begin,after);
+        s.emitlabel(after);
+    }
+
+    Stmt block() throws IOException{
+        //block->{decls stmts}
+        match('{'); Env savedEnv = top; top = new Env(top);
+        decls(); Stmt s = stmts();
+        match('}'); top = savedEnv;
+        return s;
+    }
+    void decls() throws IOException{
+        while(look.tag == Tag.BASIC) {
+            // D->type ID;
+            Type p = type();
+            Token tok = look;
+            match(Tag.ID);
+            match(';');
+            Id id = new Id((Word)tok, p, used);
+            top.put(tok, id);
+            used = used + p.width;
+        }
+    }
+
+    Type type() throws IOException{
+        Type p = (Type)look;   //expect look.tag == Tag.BASIC
+        match(Tag.BASIC);
+        if (look.tag != '[') return p;  //T -> basic
+        else return dims(p);    //return array type
+    }
+
+    Type dims(Type p) throws IOException{
+        match('['); Token tok = look; match(Tag.NUM); match(']');
+        if (look.tag == '[') p=dims(p);
+        return new Array(((Num)tok).value,p);
     }
 }
